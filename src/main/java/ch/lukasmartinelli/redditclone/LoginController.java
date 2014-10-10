51c8425d04ca34630fb02b35305217b628ba123a
Lukas Martinelli <me@lukasmartinelli.ch>
2014-10-10 19:21:42 +0200

    Removed login.xhtml as login is embedded in the header

--- src/main/java/ch/lukasmartinelli/redditclone/LoginController.java
+++ src/main/java/ch/lukasmartinelli/redditclone/LoginController.java
@@ -2,7 +2,6 @@ package ch.lukasmartinelli.redditclone;
 
 import java.io.Serializable;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
